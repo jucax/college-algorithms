eaf9e1c8468e04f8f172c43a2fb9cc2223b02b6d
Juan Carlos Martinez <martinez2@southwestern.edu>
2025-04-19 13:06:21 -0500

    Finished main.cpp implementation

--- project-4/main.cpp
+++ project-4/main.cpp
@@ -26,28 +26,79 @@ std::vector<std::string> split(const std::string &s, char separator) {
 
 void dijkstra(const Digraph &g, std::string startv, std::string endv) {
     // shortest distance found so far
-    std::map<std::string, int> distances;      
+    std::map<std::string, int> distances;  
+    for (const std::string vertex : g.getVertices()) { // For each vertex in the Digraph
+        if (vertex == startv) {
+            distances[vertex] = 0; // The distances for the first node is 0
+        } else {
+            distances[vertex] = 1000000; // This represents and infinite distances for the rest of the nodes
+        }
+    }    
+
     // previous vertex in best path found so far
-    std::map<std::string, std::string> previousVertex;    
+    std::map<std::string, std::string> previousVertex; 
+
+    // add vertices to priority queue   
     PriorityQueue pq;
+    for (const std::string vertex : g.getVertices()) { // For each vertex in the Digraph
+        pq.insert(vertex, distances[vertex]); // Using the function we implemented
+    }    
 
 
     std::cout << "Run dijkstra " << startv << " " << endv << " " << g.getVertices().size() << std::endl;
 
     // Implement Dijkstra's algorithm here using the structures defined
     // above and the functionality in Digraph and PriorityQueue
+    while (!pq.isEmpty()) {
+        std::string minVertex = pq.extractMin();
+
+        std::cout << "Visiting vertex " << minVertex << std::endl;
+
+        for (const DirectedEdge& edge : g.getEdges(minVertex)) {
+        std::string neighborVertex = edge.endVertex; // Get the neighbor of the vertex we are visiting
+        int weight = edge.weight;
+
+        int updatedDistance = distances[minVertex] + weight; // Add that weight to the provious distance accumulated
+
+            if (updatedDistance < distances[neighborVertex]) { // If the new distance is better than the other alternaticves, then we update the priority queue
+                std::cout << "Updating distances[" << neighborVertex << "] from "
+                        << distances[neighborVertex] << " to " << updatedDistance << std::endl;
+
+                // Update distances
+                distances[neighborVertex] = updatedDistance;
+                previousVertex[neighborVertex] = minVertex;
+                pq.updatePriority(neighborVertex, updatedDistance);
+            }
+        }
+    }
    
+    // Calculate shortest path
+    // Create helper variables
+    std::vector<std::string> path;
+    std::string currentVertex = endv;
+
+    while (currentVertex != startv && previousVertex.find(currentVertex) != previousVertex.end()) { // Loop until we reach the start vertex
+        path.push_back(currentVertex);
+        currentVertex = previousVertex[currentVertex];
+    }
+
+    path.push_back(startv); // Don't forget to include the start vertex at the end
+    std::reverse(path.begin(), path.end()); // Reverse to get the normal direction
 
     std::cout << std::endl << "Shortest path is:" << std::endl;
     // Print out the shortest path; match formatting
+    for (const std::string& vertex : path) {
+    std::cout << vertex << " ";
+    }
 
-    std::cout << std::endl << "It has length: " << std::endl;
     // Print the length of the shortest path
+    std::cout << std::endl << "It has length: " << distances[endv] << std::endl;
 
     std::cout << "Final map of distances:" << std::endl;
     for (auto pair : distances){
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
+
     std::cout << std::endl << "Final map of previous vertices:" << std::endl;
     for (auto pair : previousVertex) {
         std::cout << pair.first << ": " << pair.second << std::endl;
@@ -64,7 +115,7 @@ int main() {
     std::string filename;
     std::cout << "Enter the filename\n";
     std::cin >> filename;
-    std::ifstream fin("Root/" + filename);
+    std::ifstream fin(filename);
     if (fin.fail()) {
         std::cerr << "Could not open file. Exiting." << std::endl;
         exit(1);
